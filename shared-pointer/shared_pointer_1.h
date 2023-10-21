dc0916fc730b25bd1e8e8b37c4b3ecb750f34d47
Simon Hrabec <simon.hrabec@gmail.com>
2023-10-21 17:51:27 +0900

    formating changes - spaces

--- shared-pointer/shared_pointer_1.h
+++ shared-pointer/shared_pointer_1.h
@@ -3,38 +3,38 @@
 template <typename T>
 class shared_pointer {
 private:
-	struct control_block {
-		control_block() = default;
+    struct control_block {
+        control_block() = default;
 
-		std::atomic<std::size_t> reference_count = 1;
-	};
+        std::atomic<std::size_t> reference_count = 1;
+    };
 
 public:
-	shared_pointer(T* data) : block(new control_block()), data(data) {
-	}
+    shared_pointer(T* data) : block(new control_block()), data(data) {
+    }
 
-	shared_pointer(const shared_pointer& other) noexcept {
-		block = other.block;
-		block->reference_count.fetch_add(1);
-		data = other.data;
-	}
+    shared_pointer(const shared_pointer& other) noexcept {
+        block = other.block;
+        block->reference_count.fetch_add(1);
+        data = other.data;
+    }
 
-	~shared_pointer() {
-		if (block->reference_count.fetch_sub(1) == 1) {
-			delete block;
-			delete data;
-		}
-	}
+    ~shared_pointer() {
+        if (block->reference_count.fetch_sub(1) == 1) {
+            delete block;
+            delete data;
+        }
+    }
 
-	T& operator*() const noexcept {
-		return *data;
-	}
+    T& operator*() const noexcept {
+        return *data;
+    }
 
-	T* operator->() const noexcept {
-		return data;
-	}
+    T* operator->() const noexcept {
+        return data;
+    }
 
 private:
-	control_block* block;
-	T* data;
+    control_block* block;
+    T* data;
 };
--- shared-pointer/shared_pointer_3.h
+++ shared-pointer/shared_pointer_3.h
@@ -3,110 +3,110 @@
 template <typename T>
 class shared_pointer {
 private:
-	struct control_block {
-		control_block() = default;
+    struct control_block {
+        control_block() = default;
 
-		std::atomic<std::size_t> reference_count = 1;
-	};
+        std::atomic<std::size_t> reference_count = 1;
+    };
 
 public:
-	shared_pointer() noexcept : block(nullptr), data(nullptr) {
-	}
+    shared_pointer() noexcept : block(nullptr), data(nullptr) {
+    }
 
-	shared_pointer(T* data) : block(new control_block()), data(data) {
-	}
+    shared_pointer(T* data) : block(new control_block()), data(data) {
+    }
 
-	shared_pointer(const shared_pointer& other) noexcept {
-		block = other.block;
-		data = other.data;
-		block->reference_count.fetch_add(1);
-	}
+    shared_pointer(const shared_pointer& other) noexcept {
+        block = other.block;
+        data = other.data;
+        block->reference_count.fetch_add(1);
+    }
 
-	shared_pointer(shared_pointer&& other) noexcept {
-		block = other.block;
-		data = other.data;
-		other.block = nullptr;
-		other.data = nullptr;
-	}
+    shared_pointer(shared_pointer&& other) noexcept {
+        block = other.block;
+        data = other.data;
+        other.block = nullptr;
+        other.data = nullptr;
+    }
 
-	~shared_pointer() {
-		check_and_release_ownership();
-	}
+    ~shared_pointer() {
+        check_and_release_ownership();
+    }
 
-	shared_pointer& operator=(const shared_pointer& other) noexcept {
-		if (block != other.block) {
-			check_and_release_ownership();
-			block = other.block;
-			data = other.data;
-			block->reference_count.fetch_add(1);
-		}
+    shared_pointer& operator=(const shared_pointer& other) noexcept {
+        if (block != other.block) {
+            check_and_release_ownership();
+            block = other.block;
+            data = other.data;
+            block->reference_count.fetch_add(1);
+        }
 
-		return *this;
-	}
+        return *this;
+    }
 
-	shared_pointer& operator=(shared_pointer&& other) noexcept {
-		if (block != other.block) {
-			check_and_release_ownership();
-		}
-		
-		block = other.block;
-		data = other.data;
-		other.block = nullptr;
-		other.data = nullptr;
+    shared_pointer& operator=(shared_pointer&& other) noexcept {
+        if (block != other.block) {
+            check_and_release_ownership();
+        }
 
-		return *this;
-	}
+        block = other.block;
+        data = other.data;
+        other.block = nullptr;
+        other.data = nullptr;
 
-	T& operator*() const noexcept {
-		return *data;
-	}
+        return *this;
+    }
 
-	T* operator->() const noexcept {
-		return data;
-	}
+    T& operator*() const noexcept {
+        return *data;
+    }
 
-	long use_count() const noexcept {
-		if (block == nullptr) {
-			return 0;
-		} else {
-			return block->reference_count;	
-		}
-	}
+    T* operator->() const noexcept {
+        return data;
+    }
 
-	explicit operator bool() const noexcept {
-		return data != nullptr;
-	}
+    long use_count() const noexcept {
+        if (block == nullptr) {
+            return 0;
+        } else {
+            return block->reference_count;  
+        }
+    }
 
-	void reset() noexcept {
-		check_and_release_ownership();
-		block = nullptr;
-		data = nullptr;
-	}
+    explicit operator bool() const noexcept {
+        return data != nullptr;
+    }
 
-	void reset(T* new_data) {
-		check_and_release_ownership();
-		block = new control_block();
-		data = new_data;
-	}
+    void reset() noexcept {
+        check_and_release_ownership();
+        block = nullptr;
+        data = nullptr;
+    }
 
-	void swap(shared_pointer& other) noexcept {
-		std::swap(block, other.block);
-		std::swap(data, other.data);
-	}
+    void reset(T* new_data) {
+        check_and_release_ownership();
+        block = new control_block();
+        data = new_data;
+    }
+
+    void swap(shared_pointer& other) noexcept {
+        std::swap(block, other.block);
+        std::swap(data, other.data);
+    }
 
 private:
-	void check_and_release_ownership() {
-		if (block != nullptr && block->reference_count.fetch_sub(1) == 1) {
-			delete block;
-			delete data;
-		}
-	}
+    void check_and_release_ownership() {
+        if (block != nullptr && block->reference_count.fetch_sub(1) == 1) {
+            delete block;
+            delete data;
+        }
+    }
 
-	control_block* block;
-	T* data;
+    control_block* block;
+    T* data;
 };
 
 template <typename T, typename... Args>
 inline shared_pointer<T> make_shared_pointer(Args&&... args) {
-	return shared_pointer<T>(new T(std::forward<Args>(args)...));
+    return shared_pointer<T>(new T(std::forward<Args>(args)...));
 }
